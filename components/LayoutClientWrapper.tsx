087b80810bb11eeaa2de6b8a68919ae3d18b2633
Sonam Sherpa <sherpa.sjs@gmail.com>
2026-06-09 10:47:41 -0700

    fixed deployment error

--- components/LayoutClientWrapper.tsx
+++ components/LayoutClientWrapper.tsx
@@ -1,18 +1,14 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import MobileBottomBarWrapper from './MobileBottomBarWrapper';
-import AIAssistant from './AIAssistant';
 import ServiceWorkerRegistration from './ServiceWorkerRegistration';
 
 const LayoutClientWrapper: React.FC = () => {
-  const [isAIChatOpen, setIsAIChatOpen] = useState(false);
-
   return (
     <>
       <ServiceWorkerRegistration />
-      <MobileBottomBarWrapper onAIChatToggle={() => setIsAIChatOpen(true)} />
-      <AIAssistant isOpen={isAIChatOpen} onToggle={setIsAIChatOpen} />
+      <MobileBottomBarWrapper />
     </>
   );
 };
